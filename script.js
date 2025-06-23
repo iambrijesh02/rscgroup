df2fb6e432c10c131392e09f9ce6451c2c8ab767
iambrijesh02 <contact.brijeshkumar02@gmail.com>
2025-06-23 08:30:17 +0530

    RSC Group website upload

---
+++ script.js
@@ -0,0 +1,41 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const counters = document.querySelectorAll(".counter");
+
+    const animateCounter = (el, target) => {
+      let count = 0;
+      const step = Math.ceil(target / 100);
+      const update = () => {
+        count += step;
+        if (count < target) {
+          el.innerText = count;
+          requestAnimationFrame(update);
+        } else {
+          el.innerText = target;
+        }
+      };
+      update();
+    };
+
+    const observer = new IntersectionObserver(
+      (entries, obs) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const el = entry.target;
+            el.classList.add("visible");
+            const target = +el.getAttribute("data-target");
+            animateCounter(el, target);
+            obs.unobserve(el);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    counters.forEach((counter) => {
+      observer.observe(counter);
+    });
+  });
+
+  setInterval(() => { if (!$('#investmentFormModal').hasClass('show')) $('#investmentFormModal').modal('show'); }, 20000);
+
